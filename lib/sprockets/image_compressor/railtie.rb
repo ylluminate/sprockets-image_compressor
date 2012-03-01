7a844f913ece41269083dfa57821b59462da3723
George Plymale <george@orbitalimpact.com>
2012-02-29 21:12:40 -0500

    where am i

--- lib/sprockets/image_compressor/railtie.rb
+++ lib/sprockets/image_compressor/railtie.rb
@@ -2,6 +2,7 @@ module Sprockets
   module ImageCompressor
     class Railtie < Rails::Engine
       initializer :setup_image_compressors do |app|
+        puts "Inside: ${__FILE__}"
         Integration.setup app.assets if app.config.assets.compress
       end
     end
